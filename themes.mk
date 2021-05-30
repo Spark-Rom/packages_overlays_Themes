7d1ea339f639e3be078e46b56e24bb701ca46f5c
spkal01 <kalligeross@gmail.com>
2021-05-30 17:30:08 +0530

    Themes: Drop fonts

--- themes.mk
+++ themes.mk
@@ -12,41 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Fonts
-LOCAL_PATH := packages/overlays/Themes/fonts
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ttf,$(TARGET_COPY_OUT_PRODUCT)/fonts)
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
-
-#Font Overalys
-PRODUCT_PACKAGES += \
-    FontArbutusSourceOverlay \
-    FontArvoLatoOverlay \
-    FontGoogleSansOverlay \
-    FontRubikRubikOverlay \
-    FontAdamCGinora \
-    FontBigNoodle \
-    FontBikoHanken \
-    FontMittelschrift \
-    FontPissel \
-    FontReemKufi \
-    FontRoboto \
-    FontAclonicaSourceOverlay \
-    FontAmaranteSourceOverlay \
-    FontBariolSourceOverlay \
-    FontCagliostroSourceOverlay \
-    FontComfortaaSourceOverlay \
-    FontComicSansSourceOverlay \
-    FontCoolstorySourceOverlay \
-    FontLGSmartGothicSourceOverlay \
-    FontOneplusSlateSource \
-    FontRosemarySourceOverlay \
-    FontSamsungOneSourceOverlay \
-    FontSonySketchSourceOverlay \
-    FontSurferSourceOverlay
-
-# Icon Shapes
 PRODUCT_PACKAGES += \
     IconShapeCloudyOverlay \
     IconShapeCylindricalOverlay \
